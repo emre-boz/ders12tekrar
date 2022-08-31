b53dc3cd148859f1144492251d0ac61a5911f7d3
Emre Boz <emrento@gmail.com>
2022-08-31 12:45:08 +0300

    css inline external oluşturuldu

---
+++ src/components/C1.js
@@ -0,0 +1,20 @@
+import "./C1.css";
+
+
+const paragrafStili={
+    color:"red",
+    backgroundColor:"black",
+    padding: "1rem"
+}
+
+
+function C1(){
+    return (
+        <>
+        <p style={paragrafStili}>lorem illde lorem</p>
+        <p className="ozel-paragraf">lorem illde lorem</p>
+        </>
+    )
+}
+
+export default C1;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import C1 from "./components/C1"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<p style={{color:"red"}}>merhaba react 😊</p>
+    <>
+    <p style={{color:"red"}}>merhaba react 😊</p>
+    <C1 />
+    </>
+
 );
 
